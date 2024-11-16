c5df34b6bb8979390ed9cd7fb3c11ce1b60b0710
luotianxiong <1494693819@qq.com>
2024-11-16 19:43:42 +0800

    第一次提交测试

---
+++ helloGit.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+int main()
+{
+  std::vector<int>twoAdd(const std::vector<int>&a,const std::vector<int>&b);
+    std::vector<int>V1={1,2,3,4,5};
+    std::vector<int>V2={1,1,1,1,1};//赋值
+    std::vector<int>V4=twoAdd(V1,V2);
+    V4.insert(V4.begin(),V1.front());
+    std::cout<<V1[0]<<std::endl;
+    return 0;
+}
+
+std::vector<int>twoAdd(const std::vector<int>&a,const std::vector<int>&b)
+{
+   std::vector<int>C;
+   C.resize(a.size());
+   for(int i=0;i<a.size();i++)
+   {
+    C[i]=a[i]+b[i];
+   }
+   return C;
+   
+}
